f3231278ebdfbcae95d7c8646e7c0760e2113e35
MeilynArchila <meilyn.archila.202302057@gmail.com>
2025-11-09 01:57:42 -0600

    Archivos agregados de Estudiantes y Grados

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,19 @@ $routes->get('/', 'Home::index');
 $routes->post('iniciar_sesion', 'UsuariosController::index');
 
 $routes->get('cerrar_sesion', 'UsuariosController::cerrarSesion');
+
+// Empleados
+$routes->get('empleados', 'EmpleadosController::index');
+$routes->get('eliminar_empleado/(:num)', 'EmpleadosController::eliminarEmpleado/$1');
+$routes->get('buscar_empleado/(:num)', 'EmpleadosController::buscarEmpleado/$1');
+
+$routes->post('agregar_empleado', 'EmpleadosController::agregarEmpleado');
+$routes->post('modificar_empleado', 'EmpleadosController::modificarEmpleado');
+
+// grados
+$routes->get('grados', 'GradosController::index');
+$routes->get('eliminar_grado/(:num)', 'GradosController::eliminarGrado/$1');
+$routes->get('buscar_grado/(:num)', 'GradosController::buscarGrado/$1');
+
+$routes->post('agregar_grado', 'GradosController::agregarGrado');
+$routes->post('modificar_grado', 'GradosController::modificarGrado');

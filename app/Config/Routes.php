5313a04839a0b2b3fb04c7064438f494b4717373
MeilynArchila <meilyn.archila.202302057@gmail.com>
2025-11-09 13:47:00 -0600

    Modificación en rutas y en rama

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -27,4 +27,10 @@ $routes->get('buscar_grado/(:num)', 'GradosController::buscarGrado/$1');
 $routes->post('agregar_grado', 'GradosController::agregarGrado');
 $routes->post('modificar_grado', 'GradosController::modificarGrado');
 
+// Estudiantes
+$routes->get('estudiantes', 'EstudiantesController::index');
+$routes->get('eliminar_estudiante/(:num)', 'EstudiantesController::eliminarEstudiante/$1');
+$routes->get('buscar_estudiante/(:num)', 'EstudiantesController::buscarEstudiante/$1');
+
+$routes->post('agregar_estudiante', 'EstudiantesController::agregarEstudiante');
 $routes->post('modificar_estudiante', 'EstudiantesController::modificarEstudiante');
--- app/Controllers/EstudiantesController.php
+++ app/Controllers/EstudiantesController.php
@@ -1,49 +1,61 @@
+<?php
 
-public function index(): string
+namespace App\Controllers;
+use App\Models\EstudiantesModel;
+
+class EstudiantesController extends BaseController
 {
-    $estudiantes = new EstudiantesModel();
-    $datos['datos'] = $estudiantes->findAll();
-    return view('estudiantes', $datos);
-}
+    public function index(): string
+    {
+        $estudiantes = new EstudiantesModel();
+        $datos['datos'] = $estudiantes->findAll();
+        return view('estudiantes', $datos);
+    }
+    public function agregarEstudiante()
+    {
+        $estudiantes = new EstudiantesModel();
+        $datos = [
+            'carne_alumno' => $this->request->getPost('txt_carne_alumno'),
+            'nombre' => $this->request->getPost('txt_nombre'),
+            'apellido' => $this->request->getPost('txt_apellido'),
+            'direccion' => $this->request->getPost('txt_direccion'),
+            'telefono' => $this->request->getPost('txt_telefono'),
+            'email' => $this->request->getPost('txt_email'),
+            'fechanacimiento' => $this->request->getPost('txt_fechanacimiento'),
+            'codigo_grado' => $this->request->getPost('txt_codigo_grado')
+        ];
+        $estudiantes->insert($datos);
+        return $this->index();
+    }
 
+    public function eliminarEstudiante($carne_alumno)
+    {
+        $estudiantes = new EstudiantesModel();
+        $estudiantes->delete($carne_alumno);
+        return $this->index();
+    }
 
-public function agregarEstudiante()
-{
-    $estudiantes = new EstudiantesModel();
-    $datos = [
-        'carne_alumno' => $this->request->getPost('txt_carne_alumno'),
-        'nombre' => $this->request->getPost('txt_nombre'),
-        'apellido' => $this->request->getPost('txt_apellido'),
-        'direccion' => $this->request->getPost('txt_direccion'),
-        'telefono' => $this->request->getPost('txt_telefono'),
-        'email' => $this->request->getPost('txt_email'),
-        'fechanacimiento' => $this->request->getPost('txt_fechanacimiento'),
-        'codigo_grado' => $this->request->getPost('txt_codigo_grado')
-    ];
-    $estudiantes->insert($datos);
-    return $this->index();
+    public function buscarEstudiante($carne_alumno)
+    {
+        $estudiantes = new EstudiantesModel();
+        $datos['datos'] = $estudiantes->where('carne_alumno', $carne_alumno)->first();
+        return view('form_editar_estudiante', $datos);
+    }
+    
+    public function modificarEstudiante()
+    {
+        $estudiantes = new EstudiantesModel();
+        $datos = [
+            'nombre' => $this->request->getPost('txt_nombre'),
+            'apellido' => $this->request->getPost('txt_apellido'),
+            'direccion' => $this->request->getPost('txt_direccion'),
+            'telefono' => $this->request->getPost('txt_telefono'),
+            'email' => $this->request->getPost('txt_email'),
+            'fechanacimiento' => $this->request->getPost('txt_fechanacimiento'),
+            'codigo_grado' => $this->request->getPost('txt_codigo_grado'),
+        ];
+        $codigo = $this->request->getPost('txt_carne_alumno');
+        $estudiantes->update($codigo, $datos);
+        return $this->index();
+    }
 }
-public function eliminarEstudiante($carne_alumno)
-{
-    $estudiantes = new EstudiantesModel();
-    $estudiantes->delete($carne_alumno);
-    return $this->index();
-}
-public function modificarEstudiante()
-{
-    $estudiantes = new EstudiantesModel();
-    $datos = [
-        'nombre' => $this->request->getPost('txt_nombre'),
-        'apellido' => $this->request->getPost('txt_apellido'),
-        'direccion' => $this->request->getPost('txt_direccion'),
-        'telefono' => $this->request->getPost('txt_telefono'),
-        'email' => $this->request->getPost('txt_email'),
-        'fechanacimiento' => $this->request->getPost('txt_fechanacimiento'),
-        'codigo_grado' => $this->request->getPost('txt_codigo_grado'),
-    ];
-    $codigo = $this->request->getPost('txt_carne_alumno');
-    $estudiantes->update($codigo, $datos);
-    return $this->index();
-}
-
-

d94b78d666d482f0e4fb4eb5ce9f7f7293739526
ErikaRodas <erikarodad026@gmail.com>
2025-11-09 20:44:50 -0600

    FIX: Ajustes en EstudiantesController antes de fusionar main

--- app/Controllers/EstudiantesController.php
+++ app/Controllers/EstudiantesController.php
@@ -1,16 +1,58 @@
+
 <?php
 
 namespace App\Controllers;
 use App\Models\EstudiantesModel;
 
 class EstudiantesController extends BaseController
+
+public function index(): string
+{
+    $estudiantes = new EstudiantesModel();
+    $datos['datos'] = $estudiantes->findAll();
+    return view('estudiantes', $datos);
+}
+
+
+use App\Models\EstudiantesModel;
+
+
+class EstudiantesController extends BaseController
+{
+    
+    public function index(): string
+    {
+        
+
+public function agregarEstudiante()
+
 {
     public function index(): string
     {
+
         $estudiantes = new EstudiantesModel();
         $datos['datos'] = $estudiantes->findAll();
         return view('estudiantes', $datos);
     }
+
+
+  
+    public function agregarEstudiante()
+    {
+        
+        $estudiantes = new EstudiantesModel();
+        $datos = [
+            'carne_alumno'      => $this->request->getPost('txt_carne_alumno'),
+            'nombre'            => $this->request->getPost('txt_nombre'),
+            'apellido'          => $this->request->getPost('txt_apellido'),
+            'direccion'         => $this->request->getPost('txt_direccion'),
+            'telefono'          => $this->request->getPost('txt_telefono'),
+            'email'             => $this->request->getPost('txt_email'),
+            'fechanacimiento'   => $this->request->getPost('txt_fechanacimiento'),
+            'codigo_grado'      => $this->request->getPost('txt_codigo_grado')
+        ];
+        
+       
     public function agregarEstudiante()
     {
         $estudiantes = new EstudiantesModel();
@@ -24,6 +66,7 @@ class EstudiantesController extends BaseController
             'fechanacimiento' => $this->request->getPost('txt_fechanacimiento'),
             'codigo_grado' => $this->request->getPost('txt_codigo_grado')
         ];
+
         $estudiantes->insert($datos);
         return $this->index();
     }
@@ -45,6 +88,26 @@ class EstudiantesController extends BaseController
     public function modificarEstudiante()
     {
         $estudiantes = new EstudiantesModel();
+        $codigo = $this->request->getPost('txt_carne_alumno');
+        $datos = [
+            'nombre'            => $this->request->getPost('txt_nombre'),
+            'apellido'          => $this->request->getPost('txt_apellido'),
+            'direccion'         => $this->request->getPost('txt_direccion'),
+            'telefono'          => $this->request->getPost('txt_telefono'),
+            'email'             => $this->request->getPost('txt_email'),
+            'fechanacimiento'   => $this->request->getPost('txt_fechanacimiento'),
+            'codigo_grado'      => $this->request->getPost('txt_codigo_grado'),
+        ];
+        
+        
+        $estudiantes->update($codigo, $datos);
+        return $this->index();
+    }
+}
+
+    public function modificarEstudiante()
+    {
+        $estudiantes = new EstudiantesModel();
         $datos = [
             'nombre' => $this->request->getPost('txt_nombre'),
             'apellido' => $this->request->getPost('txt_apellido'),
@@ -59,3 +122,5 @@ class EstudiantesController extends BaseController
         return $this->index();
     }
 }
+}
+
